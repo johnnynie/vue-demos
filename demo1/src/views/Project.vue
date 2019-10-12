ce979c7c69df59c313104dfff336796979282e21
niejunchan <junchan-2003@163.com>
2019-10-12 15:44:10 +0800

    相应路由参数变化demo

---
+++ demo1/src/views/Project.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h5>项目ID:{{$route.params.projectId}}</h5>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Project'
+}
+</script>
---
+++ demo1/src/views/ProjectList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h5>项目列表</h5>
+    <div v-for="item in projects" v-bind:key="item.id">
+        <router-link :to="{ name: 'project', params: { projectId: item.id }}">{{item.name}}</router-link>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Project',
+  data(){
+      return {
+          projects:[{
+              name:'项目一',
+              id:1
+          },{
+              name:'项目二',
+              id:2
+          }]
+      }
+  }
+}
+</script>
